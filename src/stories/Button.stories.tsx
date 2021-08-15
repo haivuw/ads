b077005fccddee4fe44030dff6b6e46a25ee38d3
haivh <haivh@solashi.com>
2021-08-15 16:49:36 +0700

    add stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -6,7 +6,10 @@ export default {
   title: 'Components/Button',
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' }
+    variant: {
+      options: ['outlined', 'text', 'contained'],
+      control: 'radio'
+    }
   }
 } as ComponentMeta<typeof Button>;
 
@@ -14,8 +17,16 @@ const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Base = Template.bind({});
 Base.args = {
-  children: 'ButtonText',
+  children: 'Text',
+  disabled: false,
+  variant: 'contained',
   style: {
     // backgroundColor: 'red'
   }
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+  children: 'Processing'
 };
--- src/themes.ts
+++ src/themes.ts
@@ -1,6 +1,11 @@
 import { createTheme } from '@material-ui/core';
 
 export const BaseTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF6B6B'
+    }
+  },
   props: {
     MuiButton: {
       disableElevation: true,
