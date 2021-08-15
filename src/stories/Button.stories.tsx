bbbeca0323989003603ba0c502a6a78b99bd21ac
haivh <haivh@solashi.com>
2021-08-15 16:37:14 +0700

    fix

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from 'components/Buttons';
 
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' }
