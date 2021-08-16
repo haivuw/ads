8aa94fb00177a629188c80670a30f8c88235366e
haivh <haivh@solashi.com>
2021-08-16 09:04:36 +0700

    ts

--- src/components/campaigns/Title.tsx
+++ src/components/campaigns/Title.tsx
@@ -1,32 +1,33 @@
+// @ts-nocheck
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
-import {Box} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
 import { Picker } from 'stories/DateRange.stories';
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            display: 'flex',
-            padding: 10,
-            boxShadow: '0 2px 0px 0 rgb(0 0 0 / 14%), 0 0px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)',
-        },
-    })
+  createStyles({
+    container: {
+      display: 'flex',
+      padding: 10,
+      boxShadow:
+        '0 2px 0px 0 rgb(0 0 0 / 14%), 0 0px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)'
+    }
+  })
 );
 
 export default function TitleComponent({ text }) {
-    const classes = useStyles();
-    return (
-      <div className={classes.container}>
-          <Typography variant="h5" noWrap>
-              {text}
-          </Typography>
-          <Box m={1} />
-          <Box ml="auto" display="flex" alignItems="center">
-              <Box m={1} />
-              <Picker />
-          </Box>
-      </div>
-
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <Typography variant="h5" noWrap>
+        {text}
+      </Typography>
+      <Box m={1} />
+      <Box ml="auto" display="flex" alignItems="center">
+        <Box m={1} />
+        <Picker />
+      </Box>
+    </div>
   );
 }
